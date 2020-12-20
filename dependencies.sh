21fb8649b2a07652c7417be68abaca075adb55ae
Jamiexhz <45112918+Jamiexhz@users.noreply.github.com>
2020-12-20 14:39:00 +0800

    Add files via upload

--- dependencies.sh
+++ dependencies.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
+echo "" | sudo add-apt-repository ppa:nilarimogard/webupd8
 sudo apt-get update
+sudo apt-get install android-tools-adb -y
+sudo apt-get install android-tools-fastboot -y
+sudo echo "" >> ~/.profile
+sudo echo "if [ -d "$HOME/platform-tools" ] ; then" >> ~/.profile
+sudo echo "    PATH="$HOME/platform-tools:$PATH"" >> ~/.profile
+sudo echo "fi" >> ~/.profile
+sudo echo "" >> ~/.profile
+source ~/.profile
 sudo apt-get install bc -y
 sudo apt-get install bison -y
 sudo apt-get install build-essential -y
@@ -34,3 +43,14 @@ sudo apt-get install zlib1g-dev -y
 sudo apt-get install python -y
 sudo apt-get install make -y
 sudo apt-get install ccache -y
+mkdir -p ~/bin
+mkdir -p ~/android/lineage
+curl https://storage.googleapis.com/git-repo-downloads/repo > ~/bin/repo
+chmod a+x ~/bin/repo
+sudo echo "if [ -d "$HOME/bin" ] ; then" >> ~/.profile
+sudo echo "    PATH="$HOME/bin:$PATH"" >> ~/.profile
+sudo echo "fi" >> ~/.profile
+sudo echo "" >> ~/.profile
+source ~/.profile
+git config --global user.email "jamie_xhz@163.com"
+git config --global user.name "Jamiexhz"
