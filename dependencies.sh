8b71dbeeda3fe09f84319472742ec5f54facc662
Jamiexhz <45112918+Jamiexhz@users.noreply.github.com>
2020-12-20 14:04:26 +0800

    Create dependencies.sh

---
+++ dependencies.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+sudo apt-get update
+sudo apt-get install bc -y
+sudo apt-get install bison -y
+sudo apt-get install build-essential -y
+sudo apt-get install ccache -y
+sudo apt-get install curl -y
+sudo apt-get install flex -y
+sudo apt-get install g++-multilib -y
+sudo apt-get install gcc-multilib -y
+sudo apt-get install git -y
+sudo apt-get install gnupg -y
+sudo apt-get install gperf -y
+sudo apt-get install imagemagick -y
+sudo apt-get install lib32ncurses5-dev -y
+sudo apt-get install lib32readline-dev -y
+sudo apt-get install lib32z1-dev -y
+sudo apt-get install liblz4-tool -y
+sudo apt-get install libncurses5 -y
+sudo apt-get install libncurses5-dev -y
+sudo apt-get install libsdl1.2-dev -y
+sudo apt-get install libssl-dev -y
+sudo apt-get install libxml2 -y
+sudo apt-get install libxml2-utils -y
+sudo apt-get install lzop -y
+sudo apt-get install pngcrush -y
+sudo apt-get install rsync -y
+sudo apt-get install schedtool -y
+sleep 30
+sudo apt-get install squashfs-tools -y
+sudo apt-get install xsltproc -y
+sudo apt-get install zip -y
+sudo apt-get install zlib1g-dev -y
+sudo apt-get install python -y
+sudo apt-get install make -y
+sudo apt-get install ccache -y
